f647f765390dd247b2c29f6452e49f28991cbca8
MartinKochanov <114015998+MartinKochanov@users.noreply.github.com>
2024-07-22 01:39:04 +0300

    Unnecessary classes removed

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -38,7 +38,7 @@ const Register = () => {
                 <form onSubmit={onSubmit}>
                     <div className="relative my-4">
                         <input
-                            className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+                            className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white peer"
                             placeholder=""
                             name="email"
                             id="email"
@@ -46,12 +46,12 @@ const Register = () => {
                             onChange={onChange}
                             value={values[RegistrationFormKeys.Email]}
                         />
-                        <label htmlFor="email" className="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6" >Your Email</label>
+                        <label htmlFor="email" className="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6" >Your Email</label>
                         <BiUser className="absolute top-4 right-4" />
                     </div>
                     <div className="relative my-4">
                         <input
-                            className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+                            className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white peer"
                             placeholder=""
                             id="password"
                             name="password"
@@ -60,7 +60,7 @@ const Register = () => {
                             value={values[RegistrationFormKeys.Password]}
 
                         />
-                        <label htmlFor="password" className="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6" >Your Password</label>
+                        <label htmlFor="password" className="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6" >Your Password</label>
                         {!visible ?
                             <AiOutlineLock className="absolute top-4 right-4" onClick={changeVisibiltyCLickHandler} /> :
                             <AiOutlineUnlock className="absolute top-4 right-4" onClick={changeVisibiltyCLickHandler} />
@@ -68,7 +68,7 @@ const Register = () => {
                     </div>
                     <div className="relative my-4">
                         <input
-                            className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+                            className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white peer"
                             placeholder=""
                             id="confirm-password"
                             name="confirm-password"
@@ -76,7 +76,7 @@ const Register = () => {
                             onChange={onChange}
                             value={values[RegistrationFormKeys.ConfirmPassword]}
                         />
-                        <label htmlFor="" className="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6" >Confirm Password</label>
+                        <label htmlFor="" className="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6" >Confirm Password</label>
                         {!visible ?
                             <AiOutlineLock className="absolute top-4 right-4" onClick={changeVisibiltyCLickHandler} /> :
                             <AiOutlineUnlock className="absolute top-4 right-4" onClick={changeVisibiltyCLickHandler} />
