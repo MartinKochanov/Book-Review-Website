685c5e981c114386092deb7530de30c61ab8c834
MartinKochanov <114015998+MartinKochanov@users.noreply.github.com>
2024-07-28 00:52:22 +0300

    Save guards added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,42 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
-import { AuthProvider } from './contexts/AuthContext'
+import { AuthProvider } from "./contexts/AuthContext";
 
-import Login from "./components/login/Login"
-import Register from "./components/register/Register"
-import Navigation from "./components/navigation/Navigation"
-import Logout from "./components/logout/Logout"
-import Home from "./components/home/Home"
-import Books from "./components/books/Books"
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Navigation from "./components/navigation/Navigation";
+import Logout from "./components/logout/Logout";
+import Home from "./components/home/Home";
+import Books from "./components/books/Books";
+import { BookProvider } from "./contexts/BookContext";
+import PublicRoute from "./guards/PublicRoute";
+import PrivateRoute from "./guards/PrivateRoute";
 
 function App() {
-
   return (
     <>
       <div className="bg-cover bg-coverImg">
         <AuthProvider>
-          <Navigation />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/books" element={<Books />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/logout" element={<Logout />} />
-          </Routes>
+          <BookProvider>
+            <Navigation />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/books" element={<Books />} />
+
+              <Route element={<PrivateRoute />}>
+                <Route path="/logout" element={<Logout />} />
+              </Route>
+
+              <Route element={<PublicRoute />}>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+              </Route>
+            </Routes>
+          </BookProvider>
         </AuthProvider>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
