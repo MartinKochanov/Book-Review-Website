601aa59959208445497b1fa56c57d98d16dd0abc
MartinKochanov <114015998+MartinKochanov@users.noreply.github.com>
2024-07-28 00:51:17 +0300

    Bug fixed

--- client/src/components/book/Book.jsx
+++ client/src/components/book/Book.jsx
@@ -1,32 +1,44 @@
-import { GiOpenBook } from 'react-icons/gi'
+import { GiOpenBook } from "react-icons/gi";
 
-const Book = (
-    book
-) => {
-    return (
-        <div className="p-4 max-w-sm">
-            <div className="flex rounded-lg h-full bg-gray-800 p-8 flex-col">
-                <div className="flex items-center mb-3">
-                    <div
-                        className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-                        <GiOpenBook />
-                    </div>
-                    <h2 className="text-white text-lg font-medium">{book.title}</h2>
-                </div>
-                <div>
-                    <img className="block w-96 h-96 aspect-auto" src={book.cover} alt="" />
-                </div>
-                <div className="mt-4 flex flex-col self-center items-center w-2/3 flex-grow bg-indigo-500 rounded-2xl hover:bg-indigo-800 transition">
-                    <a href="#" className="m-2 text-white inline-flex items-center">Learn More
-                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                            strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg>
-                    </a>
-                </div>
-            </div>
+const Book = ({ book }) => {
+  return (
+    <div className="p-4 max-w-sm">
+      <div className="flex rounded-lg h-full bg-gray-800 p-8 flex-col">
+        <div className="flex items-center mb-3">
+          <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+            <GiOpenBook />
+          </div>
+          <h2 className="text-white text-lg font-medium">{book.title}</h2>
         </div>
-    );
-}
+        <div>
+          <img
+            className="block w-96 h-96 aspect-auto"
+            src={book.cover}
+            alt=""
+          />
+        </div>
+        <div className="mt-4 flex flex-col self-center items-center w-2/3 flex-grow bg-indigo-500 rounded-2xl hover:bg-indigo-800 transition">
+          <a
+            href={"books/" + book._id}
+            className="m-2 text-white inline-flex items-center"
+          >
+            Learn More
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Book
+export default Book;
--- client/src/components/books/Books.jsx
+++ client/src/components/books/Books.jsx
@@ -1,27 +1,20 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import BooksLayout from "../../layouts/BooksLayout";
-import * as bookService from "../../services/bookService"
 import Book from "../book/Book";
+import BookContext from "../../contexts/BookContext";
 const Books = () => {
-
-    const [books, setBooks] = useState([]);
-    useEffect(() => {
-        bookService.getAll()
-            .then(setBooks)
-    }, [])
-
-
-    return (
-        <BooksLayout>
-            <div className="bg-transparent flex flex-col gap-8 md:flex-row md:flex-wrap items-center justify-center text-black w-5/6 shadow-lg backdrop-filter border-2 border-gray-950 rounded-lg backdrop-blur-3xl bg-opacity-0">
-                {books.length === 0 ?
-                    <div className="h-[100vh] justify-self-center">No books found!</div> :
-                    books.map(book => (<Book key={book._id} {...book} />))
-                }
-            </div>
-        </BooksLayout>
-    )
-}
-
+  const { books } = useContext(BookContext);
+  return (
+    <BooksLayout>
+      <div className="bg-transparent flex flex-col gap-8 md:flex-row md:flex-wrap items-center justify-center text-black w-5/6 shadow-lg backdrop-filter border-2 border-gray-950 rounded-lg backdrop-blur-3xl bg-opacity-0">
+        {books.length === 0 ? (
+          <div className="h-[100vh] justify-self-center">No books found!</div>
+        ) : (
+          books?.map((book) => <Book key={book._id} book={{ ...book }} />)
+        )}
+      </div>
+    </BooksLayout>
+  );
+};
 
 export default Books;
