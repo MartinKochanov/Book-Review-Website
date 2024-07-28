c3d92639bf28b79ce030828f2e97428142946b98
MartinKochanov <114015998+MartinKochanov@users.noreply.github.com>
2024-07-29 01:52:43 +0300

    Get by id functionality added

--- client/src/services/bookService.js
+++ client/src/services/bookService.js
@@ -7,3 +7,8 @@ export const getAll = async () => {
 
   return await response;
 };
+
+export const getById = async (id) => {
+  const response = await request.get(`${baseUrl}/${id}`);
+  return await response;
+};
